fcf367ae0095284aa8394d3fd5b2d55a343c9f21
Kolter Saurey <Kolter.Saurey@asirobots.com>
2023-07-18 15:38:10 -0600

    finished pagination_helper

--- pagination_helper.py
+++ pagination_helper.py
@@ -1,5 +1,4 @@
 import math
-# TODO: complete this class
 
 class PaginationHelper:
     
@@ -20,24 +19,33 @@ class PaginationHelper:
     # returns the number of items on the given page. page_index is zero based
     # this method should return -1 for page_index values that are out of range
     def page_item_count(self, page_index):
-        if page_index > 0 or page_index < self.page_count: return -1
-        elif page_index < self.page_count: return self.items_per_page
-        else: return self.item_count % self.items_per_page
+        if page_index < 0 or page_index >= self.page_count(): return -1
+        elif page_index < self.page_count() - 1: return self.items_per_page
+        else: return (self.item_count()-1) % self.items_per_page + 1
     
     # determines what page an item at the given index is on. Zero based indexes.
     # this method should return -1 for item_index values that are out of range
     def page_index(self, item_index):
-        pass
+        if item_index < 0 or item_index >= self.item_count(): return -1 
+        else: return math.floor(item_index / self.items_per_page)
 
 helper = PaginationHelper(['a','b','c','d','e','f'], 4)
-helper.page_count() # should == 2
-helper.item_count() # should == 6
-helper.page_item_count(0) # should == 4
-helper.page_item_count(1) # last page - should == 2
-helper.page_item_count(2) # should == -1 since the page is invalid
+print('Collection = ' + str(helper.collection))
+print('Items per page = ' + str(helper.items_per_page))
+print('Page count = ' + str(helper.page_count()) + ' -- Should == 2')
+print('Item count = ' + str(helper.item_count()) + ' -- Should == 6')
+print('Page item count (0) = ' + str(helper.page_item_count(0)) + ' -- Should == 4')
+print('Page item count (1) = ' + str(helper.page_item_count(1)) + ' -- Last page - should == 2')
+print('Page item count (2) = ' + str(helper.page_item_count(2)) + ' -- Should == -1 since the page is invalid')
 
 # page_index takes an item index and returns the page that it belongs on
-helper.page_index(5) # should == 1 (zero based index)
-helper.page_index(2) # should == 0
-helper.page_index(20) # should == -1
-helper.page_index(-10) # should == -1 because negative indexes are invalid
+print('Page index (0) = ' + str(helper.page_index(0)) + ' -- Should == 0 (zero based index)')
+print('Page index (5) = ' + str(helper.page_index(5)) + ' -- Should == 1 (zero based index)')
+print('Page index (2) = ' + str(helper.page_index(2)) + ' -- Should == 0')
+print('Page index (20) = ' + str(helper.page_index(20)) + ' -- Should == -1')
+print('Page index (-10) = ' + str(helper.page_index(-10)) + ' -- Should == -1 because negative indexes are invalid')
+
+# Edge case: List [1,2,3,4] with 4 items per page
+edge = PaginationHelper([1, 2, 3, 4], 4)
+print('Page count = ' + str(edge.page_count()) + ' -- Should be 1')
+print('Page item count (0) = ' + str(edge.page_item_count(0)) + ' -- Should == 4')
